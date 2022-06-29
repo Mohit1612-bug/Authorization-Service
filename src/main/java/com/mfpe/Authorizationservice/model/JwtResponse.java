0d56b361819224e00c3f7129a420ae0f69b4a4c7
unknown <mohitchaudhari1612@gmail.com>
2022-06-29 19:17:28 +0530

    first commit

---
+++ src/main/java/com/mfpe/Authorizationservice/model/JwtResponse.java
@@ -0,0 +1,31 @@
+package com.mfpe.Authorizationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class JwtResponse{
+
+	String token;
+
+	public JwtResponse() {
+		super();
+	}
+	
+	public JwtResponse(String token) {
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
